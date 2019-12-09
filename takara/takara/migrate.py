fd3c1f4da6e104cc783e290690cb1ec5adbe7171
Thomas S Hatch <thatch45@gmail.com>
2019-12-09 10:45:12 -0700

    initial pass on migrate_seal

--- takara/takara/migrate.py
+++ takara/takara/migrate.py
@@ -22,8 +22,17 @@ async def seal(hub, unit, seal, seal_raw):
     if unit not in hub.takara.UNITS:
         raise takara.exc.UnitMissingError('The named unit "{unit}" is not available')
     unit_config = hub.takara.UNITS[unit]
-
-    # Make temp unit
-    # Copy all paths with new seal to tmp unit
+    tmp_unit = os.urandom(16).hex()
+    unit_config['unit'] = tmp_unit
+    unit_config['seal'] = seal
+    unit_config['seal_raw'] = seal_raw
+    strore = unit_config['store']
+    await getattr(hub, f'takara.store.{store}.create')(**unit_config)
+    keys = await getattr(hub, f'takara.store.{store}.list')(unit)
+    for key in keys:
+        orig = await getattr(hub, f'takara.store.{store}.get')(unit, path)
+        await getattr(hub, f'takara.store.{store}.set')(tmp_unit, path, orig)
+    await getattr(hub, f'takara.store.{store}.rm_unit')(unit)
+    await getattr(hub, f'takara.store.{store}.rename_unit')(tmp_unit, unit)
     # RM original unit
     # Move tmp unit to orig name
